c0144d53f5ddb10145e0eef94736a070cf227a8a
Your Name <you@example.com>
2024-07-29 20:19:20 +0200

    first

---
+++ src/controllers/user.controller.ts
@@ -0,0 +1,16 @@
+/**
+    * @description      : 
+    * @author           : Hp
+    * @group            : 
+    * @created          : 29/07/2024 - 19:07:35
+    * 
+    * MODIFICATION LOG
+    * - Version         : 1.0.0
+    * - Date            : 29/07/2024
+    * - Author          : Hp
+    * - Modification    : 
+**/
+const controllerUser = {
+
+}
+export default controllerUser;
---
+++ src/routes/user.routes.ts
@@ -0,0 +1,18 @@
+/**
+    * @description      : 
+    * @author           : Hp
+    * @group            : 
+    * @created          : 29/07/2024 - 18:52:35
+    * 
+    * MODIFICATION LOG
+    * - Version         : 1.0.0
+    * - Date            : 29/07/2024
+    * - Author          : Hp
+    * - Modification    : 
+**/
+import { Router } from "express"
+import { get } from "http"
+const routerUser =  Router()
+// tout les utilisateurs
+getallUser('/',con)
+export default routerUser
